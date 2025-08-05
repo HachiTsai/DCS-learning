67dd8662b6c5fb4732d07eba104640bfde1140db
hachi.tsai@gmail.com <hachi.tsai@gmail.com>
2025-08-05 20:31:38 +0800

    Deployed 56a7e78a with MkDocs version: 1.6.1

--- java.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  document.querySelectorAll('.mermaid svg text').forEach(node => {
-    node.setAttribute('fill', '#333333');
-  });
-});
